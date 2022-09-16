ac97f3ed174340ceefa21c10cf70e9eb42868f27
xTCry [Vladislav Kh] <wolt6@mail.ru>
2022-09-16 04:34:28 +0300

    feat(app): added informer on app new version

--- src/models/app/app.service.ts
+++ src/models/app/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { RedisService } from '../redis/redis.service';
+import { TelegramService } from '../telegram/telegram.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    constructor(
+        private readonly redisService: RedisService,
+        private readonly telegramService: TelegramService,
+    ) {}
+
+    onModuleInit() {
+        this.checkVersion().catch(console.error);
+    }
+
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    async checkVersion() {
+        const curVer = process.env.npm_package_version;
+        const lastVer =
+            (await this.redisService.redis.get('app:last-version')) || '0.0.0';
+
+        if (lastVer !== curVer) {
+            await this.redisService.redis.set('app:last-version', curVer);
+            if (lastVer) {
+                this.telegramService.notifyAdmin(
+                    `✨ Bot updated from <code>v${lastVer}</code> to <code>v${curVer}</code>`,
+                );
+            }
+        }
+    }
 }

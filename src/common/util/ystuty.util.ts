234496664a9dcd57461dfb2c237bf51bc422843a
xTCry [Vladislav Kh] <wolt6@mail.ru>
2024-09-11 17:53:16 +0300

    chore(util): improve regexp pattern for group name

--- src/common/util/ystuty.util.ts
+++ src/common/util/ystuty.util.ts
@@ -1,6 +1,10 @@
 export const patternTeacherId = '(?<teacherId>[0-9]{2,6})';
 const patternGroupNameTemplate =
-  '?<groupName>[А-я]{2,6}(-|\\s)([0-9]{1,2}\\(?[А-я]{1,2}\\)?)(\\s?\\(?[0-9А-я\\-]{1,7}\\)?)?';
+  '?<groupName>[А-я]{2,6}' +
+  '(-|\\s)' +
+  '([0-9]{1,2})' +
+  '(\\(?[А-я]{1,2}\\)?)?' +
+  '(\\s?\\(?[0-9А-я\\-]{1,7}\\)?)?';
 export const patternGroupName = `(${patternGroupNameTemplate})`;
 export const patternGroupName0 = `(${patternGroupNameTemplate}|0)`;
 
